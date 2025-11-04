45fca4c6ff1cff4b5489bf3cbcc3fa3fdf58a3f1
princemaxi <princemaxwellugochukwu@gmail.com>
2025-11-04 23:52:30 +0100

    backend initial integration

--- backend/app/app.py
+++ backend/app/app.py
@@ -7,16 +7,6 @@ from app.core.config import settings
 app = FastAPI(title=settings.PROJECT_NAME)
 
 
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
 app.include_router(api_router, prefix="/api")
 
 @app.get("/")
